5dee8c47b70d7cf30e9e25dec4e45477efe03077
Syamsul <sam.project99@gmail.com>
2021-06-19 22:57:31 +0700

    change xmlrpc

--- client/client.py
+++ client/client.py
@@ -1,28 +1,36 @@
 #client
-import socket
+# import socket
+# import os
+
+# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# isconnect = False
+# print('Menu FTP')
+# print('/n 1.download 2.upload 3.isi server 3.quit')
+# while True:
+#     if(isconnect == False):
+#         print('Connect To Server First')
+#         print('Connectting To Server...')
+#         client.connect("26.229.87.196", 8086)
+#         isconnect = True
+#         if isconnect == True:
+#             print("Connected \n")
+#         else:
+#             print("Failed to Connect.\n")
+#     else:
+#         temp = input("pilih menu: ")
+#         if temp == 1:
+#             print('download')
+#         elif temp == 2:
+#             print('upload')
+#         elif temp == 3:
+#             print('data')
+        
+
+from xmlrpc.client import ServerProxy
 import os
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-isconnect = False
-print('Menu FTP')
-print('/n 1.download 2.upload 3.isi server 3.quit')
-while True:
-    if(isconnect == False):
-        print('Connect To Server First')
-        print('Connectting To Server...')
-        client.connect("26.229.87.196", 8086)
-        isconnect = True
-        if isconnect == True:
-            print("Connected \n")
-        else:
-            print("Failed to Connect.\n")
-    else:
-        temp = input("pilih menu: ")
-        if temp == 1:
-            print('download')
-        elif temp == 2:
-            print('upload')
-        elif temp == 3:
-            print('data')
-        
-    
+proxy = ServerProxy('26.229.87.196:4899')
+
+if __name__ == '__main__':
+    dr = os.getcwd()
+    print(proxy.ls(dr))
--- server/server.py
+++ server/server.py
@@ -1,12 +1,32 @@
 #server
-import socket
+# import socket
+# import os
+
+# ip = "26.229.87.196"
+# port = 8086
+# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server.bind((ip, port))
+# server.listen(10)
+
+# print("Server started")
+# print("Waiting for client request...")
+
+from xmlrpc.server import SimpleXMLRPCServer
 import os
 
 ip = "26.229.87.196"
-port = 8086
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((ip, port))
-server.listen(10)
+port = 4899
+server = SimpleXMLRPCServer((ip,port), logRequests=True)
 
-print("Server started")
-print("Waiting for client request...")
+def list_directory(dir):
+    return os.listdir(dir)
+
+server.register_function(list_directory, 'ls')
+
+if __name__ == '__main__':
+    try:
+        print('Serving...')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Exiting')
+
