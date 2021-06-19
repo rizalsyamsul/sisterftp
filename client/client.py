db871681472793a195a6a02ba51224c9e33cdc53
Dame <adhamsiwi.as@gmail.com>
2021-06-19 23:31:09 +0700

    Update client.py

--- client/client.py
+++ client/client.py
@@ -3,16 +3,18 @@ import os
 
 proxy = ServerProxy('http://26.229.87.196:9999')
 
-if __name__ == '__main__':
-    while True:    
-        print('Menu FTP')
-        print('/n 1.download 2.upload 3.isi server 4.quit')
-        temp = input("pilih menu: ")
-        if temp == 1:
-            print('download')
-        elif temp == 2:
-            print(proxy.Upload())
-        elif temp == 3:
-            print(proxy.ls())
-        elif temp == 4:
-            break
+
+print(proxy.ls())
+while True:    
+    print('Menu FTP')
+    print('\n 1.download 2.upload 3.isi server 4.quit')
+    print("pilih menu: ")
+    temp = int(input())
+    if temp == 1:
+        print('download')
+    elif temp == 2:
+        print(proxy.Upload())
+    elif temp == 3:
+        print(proxy.ls())
+    elif temp == 4:
+        break
