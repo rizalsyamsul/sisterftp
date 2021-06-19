3260f28717244da6bb0076be8285b966e8766b3b
Syamsul <sam.project99@gmail.com>
2021-06-19 23:22:45 +0700

    edit

--- client/client.py
+++ client/client.py
@@ -1,36 +1,18 @@
-#client
-# import socket
-# import os
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# isconnect = False
-# print('Menu FTP')
-# print('/n 1.download 2.upload 3.isi server 3.quit')
-# while True:
-#     if(isconnect == False):
-#         print('Connect To Server First')
-#         print('Connectting To Server...')
-#         client.connect("26.229.87.196", 8086)
-#         isconnect = True
-#         if isconnect == True:
-#             print("Connected \n")
-#         else:
-#             print("Failed to Connect.\n")
-#     else:
-#         temp = input("pilih menu: ")
-#         if temp == 1:
-#             print('download')
-#         elif temp == 2:
-#             print('upload')
-#         elif temp == 3:
-#             print('data')
-        
-
 from xmlrpc.client import ServerProxy
 import os
 
 proxy = ServerProxy('http://26.229.87.196:9999')
 
 if __name__ == '__main__':
-    dr = os.getcwd()
-    print(proxy.ls(dr))
+    while True:    
+        print('Menu FTP')
+        print('/n 1.download 2.upload 3.isi server 4.quit')
+        temp = input("pilih menu: ")
+        if temp == 1:
+            print('download')
+        elif temp == 2:
+            print(proxy.Upload())
+        elif temp == 3:
+            print(proxy.ls())
+        elif temp == 4:
+            break
--- server/server.py
+++ server/server.py
@@ -1,16 +1,3 @@
-#server
-# import socket
-# import os
-
-# ip = "26.229.87.196"
-# port = 8086
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server.bind((ip, port))
-# server.listen(10)
-
-# print("Server started")
-# print("Waiting for client request...")
-
 from xmlrpc.server import SimpleXMLRPCServer
 import os
 
@@ -18,10 +5,14 @@ ip = "26.229.87.196"
 port = 9999
 server = SimpleXMLRPCServer((ip,port), logRequests=True)
 
-def list_directory(dir):
+def list_directory():
+    dir = os.getcwd()
     return os.listdir(dir)
-
 server.register_function(list_directory, 'ls')
+
+def up():
+    return 'uhuy'
+server.register_function(up, 'Upload')
 
 if __name__ == '__main__':
     try:
