0507e5684e442ceab93995c11e2c33a6fe23cbdb
Syamsul <50189632+rizalsyamsul@users.noreply.github.com>
2021-06-20 15:47:03 +0700

    add upload and download
    masih pake ip localhost, tinggal coba pake ip server

--- client/client.py
+++ client/client.py
@@ -1,19 +1,51 @@
 from xmlrpc.client import ServerProxy
+import xmlrpc.client
+import xmlrpc.client as xmlrpclib
 import os
 
-proxy = ServerProxy('http://26.229.87.196:9999')
+proxy = ServerProxy('http://localhost:9999') #tinggal diganti ip server nanti
 
 
-print(proxy.ls())
-while True:    
+#fungsi untuk upload
+def up_file(name):
+    print("Uploading {}".format(name))
+    try:
+        with open(name, "rb") as handle:
+            dt = xmlrpclib.Binary(handle.read())
+            proxy.Upload(dt, name)
+    except Exception as e:
+        print(e)
+
+#fungsi untuk download
+def down_file(name):
+    print("Downloading: {}".format(name))
+    try:
+        with open(name, "wb") as handle:
+            dt = proxy.Download(name).data
+            handle.write(dt)
+            handle.close()
+    except Exception as e:
+        print(e)
+
+
+while True:
     print('Menu FTP')
-    print('\n 1.download 2.upload 3.isi server 4.quit')
+    print('1.download 2.upload 3.isi server 4.quit')
     print("pilih menu: ")
     temp = int(input())
     if temp == 1:
-        print('download')
+        print("file yang ada di server:")
+        print(proxy.ls())
+        print("file yang akan di akan di download dari server: ")
+        file = input()
+        down_file(file)
     elif temp == 2:
-        print(proxy.Upload())
+        print("file yang ada di client:")
+        dir = os.getcwd()
+        print(os.listdir(dir))
+        print("file yang akan di upload: ")
+        file = input()
+        up_file(file)
     elif temp == 3:
         print(proxy.ls())
     elif temp == 4:
--- server/server.py
+++ server/server.py
@@ -1,18 +1,39 @@
 from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.client
 import os
 
-ip = "26.229.87.196"
+ip = "localhost" #tinggal diganti ip server nanti
 port = 9999
 server = SimpleXMLRPCServer((ip,port), logRequests=True)
 
+#fungsi untuk melihat list direktori
 def list_directory():
     dir = os.getcwd()
     return os.listdir(dir)
 server.register_function(list_directory, 'ls')
 
-def up():
-    return 'uhuy'
+#fungsi untuk menerima upload dari client
+def up(data,name):
+    try:
+        with open(name, "wb") as handle:
+            f = data.data
+            handle.write(f)
+            return True
+    except Exception as e:
+        print(e)
 server.register_function(up, 'Upload')
+
+#fungsi untuk mengirim data ke client
+def down(name):
+    try:
+        with open(name, "rb") as handle:
+            return xmlrpc.client.Binary(handle.read())
+            handle.close()
+    except Exception as e:
+        print(e)
+server.register_function(down, 'Download')
+
+
 
 if __name__ == '__main__':
     try:
@@ -20,4 +41,3 @@ if __name__ == '__main__':
         server.serve_forever()
     except KeyboardInterrupt:
         print('Exiting')
-
